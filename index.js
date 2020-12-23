0b1836c4fc527705d11d9703b57bbce6ca4bbce9
Sokolov A <teopetuk@gmail.com>
2020-12-23 09:34:34 +0300

    prevent for tableId collision

--- index.js
+++ index.js
@@ -255,7 +255,9 @@ ZongJi.prototype.start = function(options = {}) {
     switch (event.getTypeName()) {
       case 'TableMap': {
         const tableMap = this.tableMap[event.tableId];
-        if (!tableMap) {
+
+        // tableId collision?
+        if (!tableMap || tableMap.columnSchemas[0].TABLE_NAME !== event.tableName) {
           this.connection.pause();
           this._fetchTableInfo(event, () => {
             // merge the column info with metadata
